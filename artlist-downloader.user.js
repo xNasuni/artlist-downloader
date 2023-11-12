7baa4f21dc8415c503297843a96f52389ebe97ab
Mia Rodriguez <62818119+xNasuni@users.noreply.github.com>
2023-11-12 12:42:46 -0500

    update to 1.2 fixing for current artlist, it's annoying now but it's the only way

--- artlist-downloader.user.js
+++ artlist-downloader.user.js
@@ -5,80 +5,30 @@
 // @author      Mia @ github.com/xNasuni
 // @match       *://*.artlist.io/*
 // @grant       none
-// @version     1.1
+// @version     1.2
+// @require https://cdn.jsdelivr.net/npm/notiflix@3.2.6/dist/notiflix-aio-3.2.6.min.js
 // @updateURL   https://github.com/xNasuni/artlist-downloader/raw/main/artlist-downloader.user.js
 // @downloadURL https://github.com/xNasuni/artlist-downloader/raw/main/artlist-downloader.user.js
 // @supportURL  https://github.com/xNasuni/artlist-downloader/issues
 // ==/UserScript==
-window.nativeFetch = window.fetch;
 
-var LastSongPlayed = null
-
-function GetRedirectedUrl(URL) {
-    return new Promise(function (Resolve, Reject) {
-        var XHR = new XMLHttpRequest()
-        XHR.open('GET', URL)
-        XHR.onload = function () {
-            Resolve(XHR.responseURL)
-        }
-        XHR.onerror = function () {
-            Reject(new Error('Failed to get redirected URL'))
-        }
-        XHR.send();
-    })
-}
-
-function GetCurrentlyPlayingTitle() {
-    return document.querySelector('div.mx-15>div.flex>div.text-white>a.truncate').innerText
-}
-
-function GetCurrentlyPlayingAuthor() {
-    return document.querySelector('div.mx-15>div.flex>div.text-gray-200>a.truncate').innerText
-}
-
-async function HandleArtifact(URL) {
-    var Yes = confirm(`Would you like to download ${GetCurrentlyPlayingTitle()} by ${GetCurrentlyPlayingAuthor()}?`)
-    if (Yes) {
-        try {
-            const BlobData = new Blob([URL], { type: 'audio/aac' })
-            const Handle = await window.showSaveFilePicker({ suggestedName: (await GetRedirectedUrl(URL)).split('/')[6], types: [{description: "AAC File (Compressed MP3)", accept: { "audio/aac": [".aac"]}}] })
+window.XMLHttpRequestOriginalOpen = XMLHttpRequest.prototype.open;
+XMLHttpRequest.prototype.open = function (method, url) {
+    //console.log(`request @ ${url} | ${method}`);
+  	if (url.startsWith('https://cms-public-artifacts.artlist.io/')) {
+        const filename = atob(url.substring(('https://cms-public-artifacts.artlist.io/').length))
+        Notiflix.Notify.success(filename, async () => {
+            const link = 'https://cms-public-artifacts.artlist.io/' + filename
+            const BlobData = new Blob([link], { type: 'audio/aac' })
+            const Handle = await window.showSaveFilePicker({ suggestedName: filename, types: [{description: "AAC File (Compressed MP3)", accept: { "audio/aac": [".aac"]}}] })
             const Writable = await Handle.createWritable()
             await Writable.write(BlobData)
             await Writable.close()
-        } catch (Err) {
-            console.error(Err)
-            window.open(URL, '_blank')
-        }
+        }, {
+            distance: '60px',
+            fontSize: '9px',
+            closeButton: true
+        });
     }
-}
-
-window.hookedFetch = async function (RequestP, HeadersP) {
-    var RequestCopy
-    var ResponseCopy
-    if (typeof RequestP == 'string') {
-
-        if (RequestP.startsWith('https://cms-public-artifacts.artlist.io/') && RequestP != LastSongPlayed) { // Prevent Spam
-            LastSongPlayed = RequestP
-            HandleArtifact(RequestP)
-        }
-
-        RequestCopy = RequestP
-        RequestCopy = new Request(RequestP, HeadersP)
-        ResponseCopy = await window.nativeFetch(RequestCopy)
-        ResponseCopy.requestInputObject = RequestCopy
-    } else {
-        ResponseCopy = await window.nativeFetch(RequestP, HeadersP)
-    }
-    if (typeof RequestP == 'object') {
-        ResponseCopy.requestInputObject = RequestP
-    } else {
-        ResponseCopy.requestInputURL = RequestP
-        ResponseCopy.requestInputObject = RequestCopy
-    }
-
-    if (HeadersP) { ResponseCopy.requestInputHeaders = HeadersP }
-
-    return ResponseCopy
-
-}
-window.fetch = window.hookedFetch
+    window.XMLHttpRequestOriginalOpen.apply(this, arguments);
+};
