489aebd3cde8787995cd1d3a1306033108910522
Mia Rodriguez <62818119+xNasuni@users.noreply.github.com>
2023-12-23 21:46:01 -0500

    fix artlist 1.3 bc it was saving text data instead of actual audio file

--- artlist-downloader.user.js
+++ artlist-downloader.user.js
@@ -5,7 +5,7 @@
 // @author      Mia @ github.com/xNasuni
 // @match       *://*.artlist.io/*
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @require https://cdn.jsdelivr.net/npm/notiflix@3.2.6/dist/notiflix-aio-3.2.6.min.js
 // @updateURL   https://github.com/xNasuni/artlist-downloader/raw/main/artlist-downloader.user.js
 // @downloadURL https://github.com/xNasuni/artlist-downloader/raw/main/artlist-downloader.user.js
@@ -19,7 +19,10 @@ XMLHttpRequest.prototype.open = function (method, url) {
         const filename = atob(url.substring(('https://cms-public-artifacts.artlist.io/').length))
         Notiflix.Notify.success(filename, async () => {
             const link = 'https://cms-public-artifacts.artlist.io/' + filename
-            const BlobData = new Blob([link], { type: 'audio/aac' })
+            
+            let blobDataFromURL = await fetch(link).then(r => r.blob());
+            
+            const BlobData = new Blob([blobDataFromURL], { type: 'audio/aac' })
             const Handle = await window.showSaveFilePicker({ suggestedName: filename, types: [{description: "AAC File (Compressed MP3)", accept: { "audio/aac": [".aac"]}}] })
             const Writable = await Handle.createWritable()
             await Writable.write(BlobData)
